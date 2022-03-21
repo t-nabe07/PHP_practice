6e49880652499e55e27426ce84b7c9554fac3b3c
t-nabe07 <watanabe.t.job@gmail.com>
2022-03-21 16:52:33 +0900

    paraactice

---
+++ work/practice/practice.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = 'tatsuya';
+
+// echo "It's Sunday. Hello" . $name . PHP_EOL;
+// echo "It's Sunday. Hello $name" . PHP_EOL;
+// echo "It's \"Sunday\". Hello" $name" . PHP_EOL;
+echo "It's \"Sunday\".\t Hello" $name" . PHP_EOL;
